e4bcec1258b4d02298886f598ab53e5377216ead
Divyanshu <dnd.mein@gmail.com>
2026-01-02 11:18:11 +0530

    Implemented product listing and creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import {SignedIn, SignedOut, SignInButton, SignOutButton} from "@clerk/clerk-react"
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/clerk-react";
 import Navbar from "./components/Navbar";
-import {Routes,Route} from 'react-router'
+import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import ProfilePage from "./pages/ProfilePage";
@@ -10,22 +15,26 @@ import EditProductPage from "./pages/EditProductPage";
 import useAuthReq from "./hooks/useAuthReq";
 import useUserSync from "./hooks/useUserSync";
 const App = () => {
-  const {isClerkLoaded,isSignedIn}=useAuthReq();
+  const { isClerkLoaded, isSignedIn } = useAuthReq();
   useUserSync();
-  if(!isClerkLoaded)return null;
-  return <div className="min-h-screen bg-base-100" >
-    <Navbar />
-    <main className="max-w-5xl mx-auto px-4 py-8">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-                <Route path="/product/:id" element={<ProductPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/create" element={<CreatePage />} />
-                <Route path="/edit" element={<EditProductPage />} />
-
-      </Routes>
-    </main>
-    </div>;
+  if (!isClerkLoaded) return null;
+  return (
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route
+            path="/create"
+            element={isSignedIn ? <CreatePage /> : <Navigate to="/" />}
+          />
+          <Route path="/edit" element={<EditProductPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
