16a10e05a66420f17fe387dafb90ee2017bfe134
Divyanshu <dnd.mein@gmail.com>
2026-01-03 11:37:29 +0530

    Add production deployment configuration with static file serving

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+
 import { clerkMiddleware } from '@clerk/express';
+import { ENV } from './config/env';
+
+
 import userRoutes from './routes/userRoutes'
 import productRoutes from './routes/productRoutes'
 import commentRoutes from './routes/commentRoutes'
-import { ENV } from './config/env';
+
 
 
 const app=express();
 const PORT=ENV.PORT||3000;
 
-app.use(cors({origin:ENV.FRONTEND_URL,credentials:true}))//credentials true allows the frontend to send cookies to the backend so that we can authenticateapp.use(clerkMiddleware());
+app.use(cors({origin:ENV.FRONTEND_URL,credentials:true}))//credentials true allows the frontend to send cookies to the backend so that we can authenticate
+app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -19,9 +25,22 @@ app.use('/api/products',productRoutes);
 app.use('/api/comments',commentRoutes);
 
 
-app.get('/',(req,res)=>{
+app.get('/api/hello',(req,res)=>{
     res.send("Hello")
 })
+
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  // serve static files from frontend/dist
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // handle SPA routing - send all non-API routes to index.html - react app
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`)
 })
